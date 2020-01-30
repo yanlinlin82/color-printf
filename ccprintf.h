e70529c5bb8e8a75dc75b7487bac5fac0dac7a30
Linlin Yan <yanlinlin82@gmail.com>
2020-01-30 19:45:56 +0800

    Add C++ version support

--- ccprintf.h
+++ ccprintf.h
@@ -6,6 +6,9 @@
 #include <iostream>
 #endif
 
+namespace cc // colorized characters
+{
+
 #define COLOR_NORMAL  "\x1B[0m"
 #define COLOR_RED     "\x1B[31m"
 #define COLOR_GREEN   "\x1B[32m"
@@ -14,3 +17,23 @@
 #define COLOR_MAGENTA "\x1B[35m"
 #define COLOR_CYAN    "\x1B[36m"
 #define COLOR_WHITE   "\x1B[37m"
+
+#ifdef __cplusplus
+	class normal_t {}; void normal (normal_t ) { }
+	class red_t    {}; void red    (red_t    ) { }
+	class green_t  {}; void green  (green_t  ) { }
+	class yellow_t {}; void yellow (yellow_t ) { }
+	class blue_t   {}; void blue   (blue_t   ) { }
+	class magenta_t{}; void magenta(magenta_t) { }
+	class cyan_t   {}; void cyan   (cyan_t   ) { }
+	class white_t  {}; void white  (white_t  ) { }
+	std::ostream& operator << (std::ostream& os, void(*)(normal_t )) { return (os << COLOR_NORMAL ); }
+	std::ostream& operator << (std::ostream& os, void(*)(red_t    )) { return (os << COLOR_RED    ); }
+	std::ostream& operator << (std::ostream& os, void(*)(green_t  )) { return (os << COLOR_GREEN  ); }
+	std::ostream& operator << (std::ostream& os, void(*)(yellow_t )) { return (os << COLOR_YELLOW ); }
+	std::ostream& operator << (std::ostream& os, void(*)(blue_t   )) { return (os << COLOR_BLUE   ); }
+	std::ostream& operator << (std::ostream& os, void(*)(magenta_t)) { return (os << COLOR_MAGENTA); }
+	std::ostream& operator << (std::ostream& os, void(*)(cyan_t   )) { return (os << COLOR_CYAN   ); }
+	std::ostream& operator << (std::ostream& os, void(*)(white_t  )) { return (os << COLOR_WHITE  ); }
+#endif
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "ccprintf.h"
+using namespace std;
 
 int main()
 {
@@ -9,6 +10,15 @@ int main()
 	printf(COLOR_MAGENTA "This is a text line in MAGENTA.\n");
 	printf(COLOR_CYAN    "This is a text line in CYAN.\n");
 	printf(COLOR_WHITE   "This is a text line in WHITE.\n");
-	printf(COLOR_NORMAL  "This is a text line that turns back to normal.\n");
+	printf(COLOR_NORMAL  "This is a text line that turns back to normal.\n\n");
+
+	cout << cc::red     << "This is a text line in RED." << endl;
+	cout << cc::green   << "This is a text line in GREEN." << endl;
+	cout << cc::yellow  << "This is a text line in YELLOW." << endl;
+	cout << cc::blue    << "This is a text line in BLUE." << endl;
+	cout << cc::magenta << "This is a text line in MAGENTA." << endl;
+	cout << cc::cyan    << "This is a text line in CYAN." << endl;
+	cout << cc::white   << "This is a text line in WHITE." << endl;
+	cout << cc::normal  << "This is a text line that turns back to normal.\n" << endl;
 	return 0;
 }
